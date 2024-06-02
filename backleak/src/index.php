4e37be9762d7468432864b89d92819da699a3afb
埃拉 <ElaBosak233@qq.com>
2024-06-02 10:19:25 +0800

    docker: backleak

---
+++ backleak/src/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>backleak</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf8">
+
+    <style>
+        body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column;
+            height: 100vh;
+            margin: 0;
+            background: linear-gradient(to bottom left, #fff, #f8f9fa);
+            font-family: 'Helvetica Neue', Arial, sans-serif;
+            color: #343a40;
+        }
+
+        img {
+            width: 300px;
+            height: 300px;
+            border-radius: 50%;
+            box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
+        }
+
+        h1 {
+            font-size: 2em;
+            text-shadow: none;
+            color: #212529;
+            width: 60%;
+            word-wrap: break-word;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>你知道^M是什么吗？在哪里经常见过呢？</h1>
+</body>
+</html>
